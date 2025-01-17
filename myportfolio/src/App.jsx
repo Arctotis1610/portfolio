5a5a32bcaeab33f64a853a69c84955fe6f33c791
Arctotis1610 <pirotoorhiroto@gmail.com>
2025-01-18 03:32:41 +0900

    ロゴ、アイコン等変更

--- myportfolio/src/App.jsx
+++ myportfolio/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         <div className="flex container mx-auto py-20 px-5 flex-col items-center">
           <div className="w-48 h-48 md:w-64 md:h-64 relative overflow-hidden rounded-full mb-8">
             <img
-              src="./icon.png"
+              src="./img/icon.png"
               alt="Profile"
               className="w-full h-full object-cover rounded-full shadow-lg border-4 border-white aspect-square"
             />
@@ -93,25 +93,33 @@ function App() {
           <h2 className="text-3xl font-bold text-center mb-12">Skills</h2>
           <div className="grid grid-cols-2 md:grid-cols-3 gap-8 max-w-3xl mx-auto">
             {[
-              { name: "HTML/CSS" },
-              { name: "JavaScript" },
-              { name: "TypeScript" },
-              { name: "React" },
-              { name: "Next.js" },
-              { name: "Tailwind CSS" },
+              { name: "HTML", icon: "/svg/html.svg" },
+              { name: "CSS", icon: "/svg/css.svg" },
+              { name: "JavaScript", icon: "/svg/js.svg" },
+              { name: "TypeScript", icon: "/svg/ts.svg" },
+              { name: "React", icon: "/svg/react.svg" },
+              { name: "Next.js", icon: "/svg/nextjs.svg" },
+              { name: "Tailwind CSS", icon: "/svg/tailwindcss.svg" },
             ].map((skill) => (
               <div
                 key={skill.name}
                 className="bg-white p-6 rounded-lg shadow-md hover:shadow-lg duration-300"
               >
-                <h3 className="text-xl font-bold mb-2">{skill.name}</h3>
-                <p className="text-blue-500">{skill.level}</p>
+                <div className="flex flex-col items-center">
+                  {skill.icon && (
+                    <img
+                      src={skill.icon}
+                      alt={skill.name}
+                      className="w-12 h-12 mb-4"
+                    />
+                  )}
+                  <h3 className="text-xl font-bold mb-2">{skill.name}</h3>
+                </div>
               </div>
             ))}
           </div>
         </div>
       </section>
-
       <footer className="text-gray-700 border-t border-gray-200">
         <div className="container mx-auto p-5 text-center">
           <p>&copy; 2025 Hiroto Murakami. All rights reserved.</p>
